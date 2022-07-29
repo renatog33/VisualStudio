d07025c3ef0249c9fdf0a02de783e7009d351fc1
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-07-28 20:17:34 -0700

    js animation

--- VSAssignment/VSAssignment/wwwroot/js/site.js
+++ VSAssignment/VSAssignment/wwwroot/js/site.js
@@ -10,3 +10,25 @@ function newColor() {
 function oldColor() {
     document.getElementById("jill_scott").style.color = "darkolivegreen";
 }
+
+let start = Date.now(); // remember start time
+
+let timer = setInterval(function () {
+    // how much time passed from the start?
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 2000) {
+        clearInterval(timer); // finish the animation after 2 seconds
+        return;
+    }
+
+    // draw the animation at the moment timePassed
+    draw(timePassed);
+
+}, 20);
+
+// as timePassed goes from 0 to 2000
+// left gets values from 0px to 400px
+function draw(timePassed) {
+    train.style.left = timePassed / 5 + 'px';
+}
